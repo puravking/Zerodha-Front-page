8796bdd8b555451dcd61aadd973b4d371c2acb7f
Purav <120137918+puravking@users.noreply.github.com>
2024-09-28 23:57:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -55,3 +55,91 @@ function greet(){
     console.log("hello world");
 }
 setTimeout(greet,3000);
+// console.log("Hello");
+// // console.log(a);
+// var a = 1;
+// a = 2;
+// let name1 = "Purav"
+// console.log("My name is "+name1);
+// let isMarried = false;
+// if(isMarried) {
+//     console.log("Married")
+// }else{
+//     console.log("Not Married")
+// }
+// console.log(a)
+// let sum = 0;
+// for(let i = 0;i<=10000;i++){
+//     sum +=i;
+// }
+// console.log(sum);
+// const age = [19,23,45];
+// console.log(age[1]);
+// const user1 = {
+//     name1: "purav",
+//     age: 18
+// }
+// console.log(user1["name1"])
+// const allUsers = [{
+//     name:"pranav",
+//     age:18
+// },{
+//     name:"smruti",
+//     age:19
+// },{
+//     name:"akshit",
+//     age:19
+// }]
+// for(let i = 0;i<allUsers.length;i++){
+//     console.log(allUsers[i]["age"]);
+// }
+// function find(a,b){
+//     return a+b;
+// }
+// const val = find(23,1);
+// console.log(val);
+
+// function sum(num1,num2,fn){{
+
+//     let result = num1+num2;
+//     fn(result);
+// }
+// function displaySum(sum){
+//     console.log(sum);
+// }
+// const ans = sum(1,2,displaySum);
+
+// function greet(){
+//     console.log("hello world");
+// }
+// setTimeout(greet,3000);
+// class Animals{
+//     constructor(name,legCount,speaks){
+//         this.name = name;
+//         this.legCount = legCount;
+//         this.speaks = speaks;
+//     }
+//     static myType(){
+//         console.log("Animal");
+//     }
+//     speak(){
+//         console.log("hi, there "+ this.speaks)
+//     }
+// }
+// Animals.myType();
+// let dog = new Animals("Dog",4,"barking");
+// dog.speak();
+
+
+// function calculateSum(){
+//     for(let i =0;i<10000000000000;i++){
+
+//     }   return 0; 
+// }
+// const beforeTime = new Date();
+// const bt = beforeTime.getTime();
+// calculateSum();
+// // setTimeout(calculateSum,1000)
+// const afteTime = new Date();
+// const bat = beforeTime.getTime();
+// console.log(bat-bt);
