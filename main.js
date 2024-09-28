8fcb15dca2d767d5cd15abb2ed906bf97175ba3b
Purav <120137918+puravking@users.noreply.github.com>
2024-09-28 17:55:57 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,57 @@
+// console.log("Hello");
+// // console.log(a);
+// var a = 1;
+// a = 2;
+// let name1 = "Purav"
+// console.log("My name is "+name1);
+// let isMarried = false;
+// if(isMarried) {
+//     console.log("Married")
+// }else{
+//     console.log("Not Married")
+// }
+// console.log(a)
+// let sum = 0;
+// for(let i = 0;i<=10000;i++){
+//     sum +=i;
+// }
+// console.log(sum);
+// const age = [19,23,45];
+// console.log(age[1]);
+// const user1 = {
+//     name1: "purav",
+//     age: 18
+// }
+// console.log(user1["name1"])
+// const allUsers = [{
+//     name:"pranav",
+//     age:18
+// },{
+//     name:"smruti",
+//     age:19
+// },{
+//     name:"akshit",
+//     age:19
+// }]
+// for(let i = 0;i<allUsers.length;i++){
+//     console.log(allUsers[i]["age"]);
+// }
+// function find(a,b){
+//     return a+b;
+// }
+// const val = find(23,1);
+// console.log(val);
+
+// function sum(num1,num2,fn){
+//     let result = num1+num2;
+//     fn(result);
+// }
+// function displaySum(sum){
+//     console.log(sum);
+// }
+// const ans = sum(1,2,displaySum);
+
+function greet(){
+    console.log("hello world");
+}
+setTimeout(greet,3000);
